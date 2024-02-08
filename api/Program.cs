342716c2dc2ca2a72642c068c4696c1897f5f4b6
Davi Bernardo <davi.bernardo@sbahq.org>
2024-02-08 17:46:43 -0300

    Primeira API - Trabalhando com endpoints

--- api/Program.cs
+++ api/Program.cs
@@ -2,5 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World 2!");
+app.MapPost("/", () => new {
+    name = "Davi Bernado", age = 39
+});
+app.MapGet("/AddHeader", (HttpResponse response) => {
+        response.Headers.Add("Teste", "Davi Bernardo");
+        return new {
+                name = "Davi Bernado", age = 39
+            };
+    });
 
 app.Run();
