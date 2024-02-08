93c581bfad0a7aab59081044cb8f98d542fa8668
Davi Bernardo <davi.bernardo@sbahq.org>
2024-02-08 18:25:43 -0300

    Primeira API - Parâmetro pelo body

--- api/Program.cs
+++ api/Program.cs
@@ -6,10 +6,19 @@ app.MapPost("/", () => new {
     name = "Davi Bernado", age = 39
 });
 app.MapGet("/AddHeader", (HttpResponse response) => {
-        response.Headers.Add("Teste", "Davi Bernardo");
-        return new {
-                name = "Davi Bernado", age = 39
-            };
-    });
+    response.Headers.Add("Teste", "Davi Bernardo");
+    return new {
+            name = "Davi Bernado", age = 39
+        };
+});
+
+app.MapPost("/saveproduct", (Product product) => {
+    return product.Code + " - " + product.Name;
+});
 
 app.Run();
+
+public class Product {
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
