77c920b69621924a1487947727f294e89dc84fac
Davi Bernardo <davi.bernardo@sbahq.org>
2024-02-09 16:19:27 -0300

    Dados de configuração - Arquivo de configuração

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapGet("/", () => "Hello World!");
 
@@ -32,15 +34,22 @@ app.MapDelete("/products/{code}", ([FromRoute] string code) => {
     return Results.Ok();
 });
 
+app.MapGet("/configuration/database", (IConfiguration configuration) => {
+    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
 app.Run();
 
 
 public static class ProductRepository {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration){
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product) {
-        if(Products == null)
-            Products = new List<Product>();
         Products.Add(product);
     }
 
